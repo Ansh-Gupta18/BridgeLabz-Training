8119e6933fb3abbd153a94f6b3e310a547b3faf5
Ansh Gupta <anshgupta.katera121@gmail.com>
2025-12-16 21:29:32 +0530

    [Ansh] : Added a program to find the perimeter of rectangle

---
+++ gcr-codebase/program-prerequisite/PerimeterOfRectangle.java
@@ -0,0 +1,8 @@
+public class PerimeterOfRectangle{
+public static void main(String args[]){
+double length = 10.1;
+double width = 11.2;
+double perimeter = 2 * (length + width);
+System.out.println("The perimeter of rectangle is : " +perimeter);
+}
+}
