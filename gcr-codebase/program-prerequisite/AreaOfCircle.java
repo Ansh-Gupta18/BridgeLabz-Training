e816eed244f7999cf7c98894975da662b290e38c
Ansh Gupta <anshgupta.katera121@gmail.com>
2025-12-16 17:00:44 +0530

    [Ansh]: Added program to find the area of circle

---
+++ gcr-codebase/program-prerequisite/AreaOfCircle.java
@@ -0,0 +1,7 @@
+public class AreaOfCircle{
+public static void main(String [] args){
+float radius = 10;
+double Area = 3.14 * radius * radius;
+System.out.println("The Area of Circle is :"+ Area);
+}
+}
