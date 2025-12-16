f6a19036e99863e8acdcd2f1d1b6358a99e33508
Ansh Gupta <anshgupta.katera121@gmail.com>
2025-12-16 21:31:50 +0530

    [Ansh] : Added a program to find the volume of cylinder

---
+++ gcr-codebase/program-prerequisite/VolumeOfCylinder.java
@@ -0,0 +1,9 @@
+public class VolumeOfCylinder{
+public static void main(String[] args){
+float height = 10;
+float radius = 10;
+double pi = 3.14;
+double volume = pi*radius*radius*height;
+System.out.println("The volume of cylinder is :" +volume);
+}
+}
