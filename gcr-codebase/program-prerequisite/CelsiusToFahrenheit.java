0648f80f58624a71c648a0f13f4a9c8cce795d91
Ansh Gupta <anshgupta.katera121@gmail.com>
2025-12-16 16:59:25 +0530

    [Ansh]: Added program of celcius to fahrenheit conversion

---
+++ gcr-codebase/program-prerequisite/CelsiusToFahrenheit.java
@@ -0,0 +1,7 @@
+public class CelsiusToFahrenheit{
+public static void main(String args[]){
+float Celsius =  20;
+float Fahrenheit = (Celsius * 9/5) + 32;
+System.out.println("celsius to fahrenheit is :" +Fahrenheit);
+}
+}
