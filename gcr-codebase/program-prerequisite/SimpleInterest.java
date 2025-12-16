b3907248dfa3c662b2f897d3870d5eecabe23548
Ansh Gupta <anshgupta.katera121@gmail.com>
2025-12-16 21:30:35 +0530

    [Ansh] : Added a program to find the simpleinterest

---
+++ gcr-codebase/program-prerequisite/SimpleInterest.java
@@ -0,0 +1,9 @@
+public class SimpleInterest{
+public static void main(String args[]){
+int time = 10;
+float rate = 5;
+float principal = 200;
+double SimpleInterest = (principal * rate * time) / 100;
+System.out.println(" Simple Interest is : " +SimpleInterest);
+}
+}
