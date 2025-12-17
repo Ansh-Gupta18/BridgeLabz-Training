e8fe12fc61728921e95fb723d208283bdc9c111e
Ansh Gupta <anshgupta.katera121@gmail.com>
2025-12-17 15:10:40 +0530

    [Ansh] : Added a program to find the pens distribution and remaining pens

---
+++ gcr-codebase/programming-elements/Level-1-programming/DividePens.java
@@ -0,0 +1,9 @@
+public class DividePens{
+	public static void main(String args[]){
+	double pens = 14;
+	double students = 3;
+	double studentGetPens = 14/3;
+	double remainingPens = 14%3;
+	System.out.println("The Pen Per Student is : " +studentGetPens+ " and the remaining pen no distributed is : " +remainingPens);
+}
+}
