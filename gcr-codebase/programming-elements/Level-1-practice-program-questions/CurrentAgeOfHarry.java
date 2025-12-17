20f6bb9dd5dfd99ce20da4cbfd214ff633acb51a
Ansh Gupta <anshgupta.katera121@gmail.com>
2025-12-17 12:37:04 +0530

    [Ansh] : Added a program tp find the age of harray

---
+++ gcr-codebase/programming-elements/Level-1-practice-program-questions/CurrentAgeOfHarry.java
@@ -0,0 +1,7 @@
+public class CurrentAgeOfHarry{
+ 	public static void main(String args[]){
+  	int harryBirthYear = 2000;
+ 	int harryCurrentAge = 2024;
+  	System.out.println("Harry's age in 2024 is : "+(harryCurrentAge - harryBirthYear));
+}
+}
