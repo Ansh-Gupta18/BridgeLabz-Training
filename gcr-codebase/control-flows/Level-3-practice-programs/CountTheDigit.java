736e2020b63dd01f3066e6d2c47769ff74c5ff00
Ansh Gupta <anshgupta.katera121@gmail.com>
2025-12-19 14:39:58 +0530

    [Ansh] : Added a program to count the number of digit in an integer

---
+++ gcr-codebase/control-flows/Level-3-practice-programs/CountTheDigit.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class CountTheDigit{
+	
+	public static void main(String args[]){
+	Scanner input = new Scanner(System.in);
+	// Taking input 
+	System.out.print("Enter the number : ");
+	int number = input.nextInt();
+	int count = 0;
+	while(number != 0){
+		number = number / 10;
+		count++;
+	}
+	System.out.println("count the number of digit in an integer is : " +count);
+}
+}
+
+	
