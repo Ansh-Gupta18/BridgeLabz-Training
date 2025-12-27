8eb2052e52979a8e8e2c70f5f74566c6bb9794d0
Ansh Gupta <151846706+Ansh-Gupta18@users.noreply.github.com>
2025-12-27 06:47:50 +0530

    Update EmployeeDetails.java

--- java-oops-practice/grc-codebase/JavaOopsConcepts/src/com/classandobject/level1/EmployeeDetails.java
+++ java-oops-practice/grc-codebase/JavaOopsConcepts/src/com/classandobject/level1/EmployeeDetails.java
@@ -26,5 +26,11 @@ public class EmployeeDetails {
 		
 	}
 	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Circle circle = new Circle(2.5);
+		circle.areaCircle();
+		circle.circumferenceCircle();
+		}
 
 }
