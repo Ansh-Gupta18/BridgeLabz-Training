4bb97d86e906dc79f09efb13a78de0d2b5e5076f
Ansh Gupta <anshgupta.katera121@gmail.com>
2026-01-02 15:16:23 +0530

    [Ansh] : Added a program to display the details of restaurant management system

---
+++ java-oops-practice/grc-codebase/JavaOopsConcepts/src/com/inheritance/restaurantmanagement/RestaurantPerson.java
@@ -0,0 +1,16 @@
+package com.inheritance.restaurantmanagement;
+
+public class RestaurantPerson {
+    protected String name;
+    protected int id;
+
+    public RestaurantPerson(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+	public void performDuties() {
+		
+		
+	}
+}
---
+++ java-oops-practice/grc-codebase/JavaOopsConcepts/src/com/inheritance/restaurantmanagement/Worker.java
@@ -0,0 +1,5 @@
+package com.inheritance.restaurantmanagement;
+
+public interface Worker {
+    void performDuties();
+}
