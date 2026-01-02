2adb14a6090b6c7ba16ea4da3ad29a46b95d57c2
Ansh Gupta <anshgupta.katera121@gmail.com>
2026-01-02 11:21:42 +0530

    [Ansh] : Added a program to display universityand management system

---
+++ java-oops-practice/grc-codebase/JavaOopsConcepts/src/com/objectmodelling/universitymanagementsystem/Professor.java
@@ -0,0 +1,12 @@
+package com.objectmodelling.universitymanagementsystem;
+
+//Created a Professor class
+public class Professor {
+	//private instance of class
+  private String name;
+
+  //Parameterized constructor
+  public Professor(String name) {
+      this.name = name;
+  }
+}
---
+++ java-oops-practice/grc-codebase/JavaOopsConcepts/src/com/objectmodelling/universitymanagementsystem/UniversityMain.java
@@ -0,0 +1,21 @@
+package com.objectmodelling.universitymanagementsystem;
+
+//Created a class that establish relationship between Student, professor and courses class
+public class UniversityMain {
+  public static void main(String[] args) {
+  	
+  	//Created object of students , professor and courses class
+      Students students = new Students();
+      Professor professor = new Professor("Dr. Mehta");
+      Courses course = new Courses();
+      
+      //calling method to set course name
+      course.setName("Java");
+      
+      //Calling method to enroll courses
+      students.enrollCourse(course);
+      
+      //Calling method to assign professor to courses
+      course.assignProfessor(professor);
+  }
+}
