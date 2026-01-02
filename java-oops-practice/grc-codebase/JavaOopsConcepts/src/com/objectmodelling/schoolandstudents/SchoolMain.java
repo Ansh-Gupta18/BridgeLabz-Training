c92d7a442f0b90f0ca0ff9de4af59d6bd16427c9
Ansh Gupta <anshgupta.katera121@gmail.com>
2026-01-02 11:19:19 +0530

    [Ansh] : Added a program to display school and students

---
+++ java-oops-practice/grc-codebase/JavaOopsConcepts/src/com/objectmodelling/schoolandstudents/SchoolMain.java
@@ -0,0 +1,23 @@
+package com.objectmodelling.schoolandstudents;
+
+public class SchoolMain {
+    public static void main(String[] args) {
+    	
+    	//Created object of student class
+        Student s1 = new Student("Alice");
+        Student s2 = new Student("Bob");
+
+        //Created object of Course class
+        Course java = new Course("Java");
+        Course python = new Course("Python");
+        
+        //Calling method to enroll courses
+        s1.enrollCourse(java);
+        s1.enrollCourse(python);
+        s2.enrollCourse(java);
+        
+        //Calling method to display details
+        s1.viewCourses();
+        java.showStudents();
+    }
+}
