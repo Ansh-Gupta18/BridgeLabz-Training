e9c6d813fdfabaaa4825c1a136200e498ac98b14
Ansh Gupta <anshgupta.katera121@gmail.com>
2026-01-02 11:22:39 +0530

    [Ansh] : Added a program to display faculties and departments

---
+++ java-oops-practice/grc-codebase/JavaOopsConcepts/src/com/objectmodelling/universitywithfacultiesanddepartments/Department.java
@@ -0,0 +1,17 @@
+package com.objectmodelling.universitywithfacultiesanddepartments;
+
+//Importing ArrayList from java.util package
+import java.util.ArrayList;
+import java.util.List;
+
+public class Department {
+	//Private instance of Department class
+  private String departmentName;
+ 
+  
+  //Parameterized constructor
+  public Department(String departmentName) {
+      this.departmentName = departmentName;
+  }
+ 
+}
---
+++ java-oops-practice/grc-codebase/JavaOopsConcepts/src/com/objectmodelling/universitywithfacultiesanddepartments/Faculty.java
@@ -0,0 +1,12 @@
+package com.objectmodelling.universitywithfacultiesanddepartments;
+
+//Create a class Faculty
+public class Faculty {
+	//Private instance of class
+  private String name;
+  
+  //Parameterized Constructor
+  public Faculty(String name) {
+      this.name = name;
+  }
+}
