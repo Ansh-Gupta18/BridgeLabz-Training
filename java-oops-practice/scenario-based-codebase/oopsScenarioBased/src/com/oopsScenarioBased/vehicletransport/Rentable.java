d9092e80c2a49b01e9cb96b13559a2af0d21092f
Ansh Gupta <anshgupta.katera121@gmail.com>
2026-01-03 15:18:00 +0530

    [Ansh] : Added a program to display the details of vehicle transport system

---
+++ java-oops-practice/scenario-based-codebase/oopsScenarioBased/src/com/oopsScenarioBased/vehicletransport/Rentable.java
@@ -0,0 +1,7 @@
+package com.oopsScenarioBased.vehicletransport;
+
+
+//Interface to hide payment calculation logic
+public interface Rentable {
+	double calculateRent(int days, int surcharge);
+}
---
+++ java-oops-practice/scenario-based-codebase/oopsScenarioBased/src/com/oopsScenarioBased/vehicletransport/Vehicle.java
@@ -0,0 +1,24 @@
+package com.oopsScenarioBased.vehicletransport;
+
+//Super class
+public class Vehicle implements Rentable {
+	protected int maxSpeed;
+	protected String fuelType;
+	
+	public Vehicle(int maxSpeed, String fuelType) {
+		this.maxSpeed = maxSpeed;
+		this.fuelType = fuelType;
+	}
+	
+	//Method to be override
+	public void displayInfo() {
+		System.out.println("Displaying vehicle info");
+		
+	}
+
+	@Override
+	public double calculateRent(int days, int surcharge) {
+		
+		return 0;
+	}
+}
