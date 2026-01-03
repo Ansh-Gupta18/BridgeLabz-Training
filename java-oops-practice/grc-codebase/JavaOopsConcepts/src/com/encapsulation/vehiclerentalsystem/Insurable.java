9cd3bcb5d8e7071a23d960fb87b9ad5e6e94a8bc
Ansh Gupta <anshgupta.katera121@gmail.com>
2026-01-03 12:53:02 +0530

    [Ansh] : Added a program to display the details of vehicle rental system

---
+++ java-oops-practice/grc-codebase/JavaOopsConcepts/src/com/encapsulation/vehiclerentalsystem/Insurable.java
@@ -0,0 +1,11 @@
+package com.encapsulation.vehiclerentalsystem;
+
+//Interface representing insurance-related behavior
+interface Insurable {
+
+// Method to calculate insurance cost
+double calculateInsurance(int days);
+
+// Method to get insurance details
+String getInsuranceDetails();
+}
