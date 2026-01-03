03b20fff04caf7e34573caef6f727ee8bda29e06
Ansh Gupta <anshgupta.katera121@gmail.com>
2026-01-03 15:11:43 +0530

    [Ansh] : Added a program to display the deatis of ewallet

---
+++ java-oops-practice/scenario-based-codebase/oopsScenarioBased/src/com/oopsScenarioBased/ewallet/Tranferrable.java
@@ -0,0 +1,8 @@
+package com.oopsScenarioBased.ewallet;
+
+//Interface defining money transfer capability
+interface Transferrable {
+
+// Method to transfer money to another user
+void transferTo(User receiver, double amount);
+}
