a70c7cfecf17b03ade333315c668ed9a58367343
Ansh Gupta <anshgupta.katera121@gmail.com>
2026-01-03 15:12:30 +0530

    [Ansh] : Added a program to display the details of employee account

---
+++ java-oops-practice/scenario-based-codebase/oopsScenarioBased/src/com/oopsScenarioBased/employeeaccount/CurrentAccount.java
@@ -0,0 +1,9 @@
+package com.oopsScenarioBased.employeeaccount;
+
+//CurrentAccount inherits Account
+public class CurrentAccount extends Account {
+
+public CurrentAccount(int accountNumber, double balance) {
+   super(accountNumber, balance);
+}
+}
---
+++ java-oops-practice/scenario-based-codebase/oopsScenarioBased/src/com/oopsScenarioBased/employeeaccount/Customer.java
@@ -0,0 +1,24 @@
+package com.oopsScenarioBased.employeeaccount;
+
+public class Customer {
+    private String name;
+    private Account account;
+
+    public Customer(String name, Account account) {
+        this.name = name;
+        this.account = account;
+    }
+
+    public void performDeposit(double amount) {
+        account.deposit(amount);
+    }
+
+    public void performWithdraw(double amount) {
+        account.withdraw(amount);
+    }
+
+    public void showBalance() {
+        System.out.print(name + " ");
+        account.displayBalance();
+    }
+}
