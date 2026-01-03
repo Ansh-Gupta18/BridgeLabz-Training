959a981191d5800ee829e04e1ad2c2a1dbd2ab8b
Ansh Gupta <anshgupta.katera121@gmail.com>
2026-01-03 12:47:33 +0530

    [Ansh] : Added a perogram to display the details of employee management system

---
+++ java-oops-practice/grc-codebase/JavaOopsConcepts/src/com/encapsulation/employeemanagement/FullTimeEmployee.java
@@ -0,0 +1,20 @@
+package com.encapsulation.employeemanagement;
+
+//Full-time employee class
+class FullTimeEmployee extends Employee {
+
+// Fixed monthly salary for full-time employees
+private double fixedSalary;
+
+// Constructor
+public FullTimeEmployee(int employeeId, String name, double fixedSalary) {
+   super(employeeId, name, fixedSalary);
+   this.fixedSalary = fixedSalary;
+}
+
+// Salary calculation logic for full-time employee
+@Override
+public double calculateSalary() {
+   return fixedSalary;
+}
+}
