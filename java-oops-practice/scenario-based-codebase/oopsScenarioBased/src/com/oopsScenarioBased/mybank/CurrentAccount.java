48588001517c6710b62e0bd1e7a898371cd625af
Ansh Gupta <anshgupta.katera121@gmail.com>
2026-01-05 15:08:39 +0530

    [Ansh] : Added a program to display the details of my bank

---
+++ java-oops-practice/scenario-based-codebase/oopsScenarioBased/src/com/oopsScenarioBased/mybank/CurrentAccount.java
@@ -0,0 +1,15 @@
+package com.oopsScenarioBased.mybank;
+class CurrentAccount extends Account {
+	private static final double interestRate = 10.0;
+	public CurrentAccount(int accountNumber, double balance) {
+		super(accountNumber, balance);
+
+	}
+
+	@Override
+	public void calculateInterest() {
+		double interest =  (getBalance() * interestRate) / 100;
+		System.out.println("Your Interest Rate in Current Account is = " + interest);
+	}
+
+}
---
+++ java-oops-practice/scenario-based-codebase/oopsScenarioBased/src/com/oopsScenarioBased/mybank/ITransaction.java
@@ -0,0 +1,9 @@
+package com.oopsScenarioBased.mybank;
+
+//Interface
+public interface ITransaction {
+	void deposit(double amount);
+	void withdraw(double amount);
+	void checkBalance();
+
+}
