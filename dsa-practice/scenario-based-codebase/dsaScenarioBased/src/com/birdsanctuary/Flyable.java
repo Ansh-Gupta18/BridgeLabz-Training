414c0e23623975044e81b11ae454e8ee81e63353
Ansh Gupta <anshgupta.katera121@gmail.com>
2026-01-19 14:49:16 +0530

    [Ansh] : Added a program to display the details bird sanctuary

---
+++ dsa-practice/scenario-based-codebase/dsaScenarioBased/src/com/birdsanctuary/Flyable.java
@@ -0,0 +1,9 @@
+package com.oopsScenarioBased.birdsanctuary;
+
+
+//Interface for birds that can fly
+interface Flyable {
+
+//Method for flying behavior
+void fly();
+}
---
+++ dsa-practice/scenario-based-codebase/dsaScenarioBased/src/com/birdsanctuary/Swimmable.java
@@ -0,0 +1,8 @@
+package com.oopsScenarioBased.birdsanctuary;
+
+//Interface for birds that can swim
+interface Swimmable {
+
+//Method for swimming behavior
+void swim();
+}
