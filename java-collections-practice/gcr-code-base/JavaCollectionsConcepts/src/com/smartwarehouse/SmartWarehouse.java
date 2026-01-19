bb00331176a0ee34eb6f51905da0edaf3d76400c
Ansh Gupta <anshgupta.katera121@gmail.com>
2026-01-19 16:38:41 +0530

    [Ansh] : Added a program to display the detials of smart warehouse

---
+++ java-collections-practice/gcr-code-base/JavaCollectionsConcepts/src/com/smartwarehouse/SmartWarehouse.java
@@ -0,0 +1,29 @@
+package com.smartwarehouse;
+
+public class SmartWarehouse {
+
+    public static void main(String[] args) {
+
+        Storage<Electronics> electronicsStorage = new Storage<>();
+        electronicsStorage.addItem(new Electronics("Laptop", 75000));
+        electronicsStorage.addItem(new Electronics("Mobile", 30000));
+
+        Storage<Groceries> groceriesStorage = new Storage<>();
+        groceriesStorage.addItem(new Groceries("Rice", 1200));
+        groceriesStorage.addItem(new Groceries("Wheat", 900));
+
+        Storage<Furniture> furnitureStorage = new Storage<>();
+        furnitureStorage.addItem(new Furniture("Chair", 2500));
+        furnitureStorage.addItem(new Furniture("Table", 5000));
+
+        System.out.println(" Electronics Storage:");
+        WarehouseUtil.displayItems(electronicsStorage.getItems());
+
+        System.out.println("\n Groceries Storage:");
+        WarehouseUtil.displayItems(groceriesStorage.getItems());
+
+        System.out.println("\n Furniture Storage:");
+        WarehouseUtil.displayItems(furnitureStorage.getItems());
+    }
+}
+
