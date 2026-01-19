b2a6e94689bac6cec6fcc23e0bc3f1c46243d7da
Ansh Gupta <anshgupta.katera121@gmail.com>
2026-01-19 17:19:14 +0530

    [Ansh] : Added a program to display the details of smart warehouse

---
+++ java-collections-practice/gcr-code-base/JavaCollectionsConcepts/src/com/generics/smartwarehouse/WarehouseItem.java
@@ -0,0 +1,27 @@
+package com.generics.smartwarehouse;
+
+abstract class WarehouseItem {
+    private String name;
+    private double price;
+
+    public WarehouseItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public abstract String getCategory();
+
+    @Override
+    public String toString() {
+        return getCategory() + " | Name: " + name + " | Price: ₹" + price;
+    }
+}
+
---
+++ java-collections-practice/gcr-code-base/JavaCollectionsConcepts/src/com/generics/smartwarehouse/WarehouseUtil.java
@@ -0,0 +1,12 @@
+package com.generics.smartwarehouse;
+
+import java.util.List;
+
+class WarehouseUtil {
+
+    public static void displayItems(List<? extends WarehouseItem> items) {
+        for (WarehouseItem item : items) {
+            System.out.println(item);
+        }
+    }
+}
