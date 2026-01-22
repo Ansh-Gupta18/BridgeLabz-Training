094a311918c91184f74279c3706026cc88f115bf
Ansh Gupta <anshgupta.katera121@gmail.com>
2026-01-22 23:09:54 +0530

    [Ansh] : Added a program to display the details of read file example

---
+++ java-collections-practice/gcr-code-base/JavaCollectionsConcepts/src/com/exceptionhandling/ReadFileExample.java
@@ -0,0 +1,27 @@
+package com.exceptionhandling;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFileExample {
+
+    public static void main(String[] args) {
+        String fileName = "data.txt";
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            reader.close();
+
+        } catch (IOException e) {
+            System.out.println("File not found");
+        }
+    }
+}
+
