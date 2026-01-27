bb274763c851715766b02605972908038c114af4
Ansh Gupta <anshgupta.katera121@gmail.com>
2026-01-27 11:19:44 +0530

    [Ansh] : Added a program of animal test

---
+++ java-collections-practice/gcr-code-base/JavaCollectionsConcepts/src/com/annotations/AnimalTest.java
@@ -0,0 +1,23 @@
+package com.annotations;
+
+class Animal {
+    void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+
+    @Override
+    void makeSound() {
+        System.out.println("Dog barks");
+    }
+}
+
+public class AnimalTest {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.makeSound();
+    }
+}
+
