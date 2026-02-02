860442eda3dd070557181682d45809fee3c246b0
Ansh Gupta <anshgupta.katera121@gmail.com>
2026-02-02 16:24:24 +0530

    [Ansh] : Added a program of order even use summary

---
+++ java8-practice/gcr-code-base/java8concepts/src/com/collectors/orderevenusesummary/Order.java
@@ -0,0 +1,20 @@
+package com.collectors.orderevenusesummary;
+
+public class Order {
+
+    private String customerId;
+    private double amount;
+
+    public Order(String customerId, double amount) {
+        this.customerId = customerId;
+        this.amount = amount;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+}
---
+++ java8-practice/gcr-code-base/java8concepts/src/com/collectors/orderevenusesummary/OrderRevenueSummary.java
@@ -0,0 +1,27 @@
+package com.collectors.orderevenusesummary;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class OrderRevenueSummary {
+
+    public static void main(String[] args) {
+
+        List<Order> orders = Arrays.asList(
+                new Order("CUST1", 2500),
+                new Order("CUST1", 1800),
+                new Order("CUST2", 3000),
+                new Order("CUST2", 1200)
+        );
+
+        Map<String, Double> revenue =
+                orders.stream()
+                        .collect(Collectors.groupingBy(
+                                Order::getCustomerId,
+                                Collectors.summingDouble(Order::getAmount)
+                        ));
+
+        revenue.forEach((k, v) ->
+                System.out.println(k + " → ₹" + v));
+    }
+}
