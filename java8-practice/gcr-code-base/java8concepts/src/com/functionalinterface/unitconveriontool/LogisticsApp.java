ed5263438231a4ead7baa7212771e05de65ff269
Ansh Gupta <anshgupta.katera121@gmail.com>
2026-02-02 12:59:20 +0530

    [Ansh] : Added a program of unit conversion tool

---
+++ java8-practice/gcr-code-base/java8concepts/src/com/functionalinterface/unitconveriontool/LogisticsApp.java
@@ -0,0 +1,10 @@
+package com.functionalinterface.unitconveriontool;
+
+public class LogisticsApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(UnitConverter.kmToMiles(10));
+        System.out.println(UnitConverter.kgToLbs(5));
+    }
+}
